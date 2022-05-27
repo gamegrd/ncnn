c0424cff7dc51027bd60bc011daac9b68381f468
gamegrd <gamegrd@gmail.com>
2022-05-27 13:54:36 +0800

    add makefile

--- build-linux.sh
+++ build-linux.sh
@@ -3,7 +3,7 @@
 ##### linux host system with gcc/g++
 mkdir -p build-host-gcc-linux
 pushd build-host-gcc-linux
-cmake -DNCNN_BUILD_TOOLS=ON -DCMAKE_TOOLCHAIN_FILE=../toolchains/host.gcc.toolchain.cmake ..
+cmake -DNCNN_BUILD_TOOLS=ON -DNCNN_VULKAN=ON -DCMAKE_TOOLCHAIN_FILE=../toolchains/host.gcc.toolchain.cmake ..
 make -j8
 make install
 popd
